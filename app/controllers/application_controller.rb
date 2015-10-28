9be0164a6798b07d165c62940949c413189a0d9b
Kyria <kyria.brown@gmail.com>
2015-10-27 17:02:12 -0700

    Add basic Admin functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,11 +9,15 @@ class ApplicationController < ActionController::Base
     if !current_user
       flash[:alert] = "You must log in."
       redirect_to new_session_path
-    end  
+    end
   end
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def @current_user.admin?
+    @current_user.admin == true
   end
 
   helper_method :current_user
